c2800968b4d5aac27dbe8e8f922e9c80fe24ca67
Joaquin <joacogf04@gmail.com>
2025-06-09 21:02:20 -0300

    array de destino y paquetes y condicionales

--- js/script.js
+++ js/script.js
@@ -1,24 +1,88 @@
-console.log("Hola");
+let destinos = ["Canada", "Estados Unidos", "Francia", "Italia", "China", "Portugal", "Brasil"]
 
-function saludar(nombre, edad) {
-    console.log(`Bienvenido a JavaScript, ${nombre}. Tenes ${edad} años verdad?`)
+let paquetesViajes = [
+    {
+        pais: "Canada",
+        localidad: "Vancouver",
+        estadia: "Cabaña",
+        excursion: "Kayak",
+        precio: 1250
+    },
+    {
+        pais: "Estados Unidos",
+        localidad: "New York",
+        estadia: "Hotel",
+        excursion: "Visita a museo",
+        precio: 1540
+    },
+    {
+        pais: "Francia",
+        localidad: "Paris",
+        estadia: "Hotel",
+        excursion: "Visita a torre Eifel",
+        precio: 2300
+    },
+    {
+        pais: "Italia",
+        localidad: "Roma",
+        estadia: "Hotel",
+        excursion: "Visita a coliseo romano",
+        precio: 2125
+    },
+    {
+        pais: "China",
+        localidad: "Shanhaiguan",
+        estadia: "Cabaña",
+        excursion: "Visita a la Gran Muralla China",
+        precio: 3000
+    },
+    {
+        pais: "Portugal",
+        localidad: "Lisboa",
+        estadia: "Hotel",
+        excursion: "Snorkel",
+        precio: 1700
+    }
+]
+
+
+let paquetesViajes2 = paquetesViajes.push(
+    {
+        pais: "Brasil",
+        localidad: "Rio de Janeiro",
+        estadia: "Departamento",
+        excursion: "Waterboard",
+        precio: 800
+    }
+)
+
+let edad = prompt("Cuantos años tienes?")
+
+if (edad >= 18) {
+    console.log("Podes entrar a ver los paquetes")
+    console.log(paquetesViajes)
+} else {
+    console.log("Sos menor, solo podes ver los destinos")
+    console.log(destinos)
 }
 
-saludar("Juan", 18);
 
-let edad = 12
-let apellido = prompt("Cual es su apellido?");
-console.log("Su apellido es: ", apellido)
-
-function entrarPagina() {
-    if (edad >= 18) {
-        console.log("Podes entrar a esta pagina")
-    } else (
-        console.log("Volve a probar el año que viene amigo")
-    )
+let elegirDestino;
+if (edad >= 18) {
+    elegirDestino = parseInt(prompt("Pon un maximo de presupuesto")) 
 }
 
-entrarPagina();
-
+if (elegirDestino <= 1000) {
+    const precios = paquetesViajes.filter(paquetesViajes => paquetesViajes.precio < 1000)
+    console.log(precios)
+} else if (elegirDestino <= 2000) {
+    const precios = paquetesViajes.filter(paquetesViajes => paquetesViajes.precio < 2000)
+    console.log(precios)
+} else if (elegirDestino <= 3000) {
+    const precios = paquetesViajes.filter(paquetesViajes => paquetesViajes.precio < 3000)
+    console.log(precios)
+} else if (elegirDestino >= 3001) {
+    console.log(paquetesViajes)
+}
 
 
